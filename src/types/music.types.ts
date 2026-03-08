2a0f9e5bfe48a6d6b72a84ef275d3590c7104bba
maricar <balaganmaricar3@gmail.com>
2026-03-08 20:22:22 +0800

    Added music.types.ts as datatype fetchers

---
+++ src/types/music.types.ts
@@ -0,0 +1,16 @@
+export interface UserMusic {
+  id: string;
+  title: string;
+  artist?: string;
+  audioFile: File | null;
+  audioData?: string; 
+  imageData?: string; 
+  duration?: number;
+  dateAdded: number;
+}
+
+export interface MusicPlayerHandle {
+  pause: () => void;
+  playTrack: (id: string) => void;
+  searchTrack?: (title: string) => void;
+}
