4bfd87dff2d08636b4abc820388c1c69b70f7756
maricar <balaganmaricar3@gmail.com>
2026-03-11 16:44:24 +0800

    Updated Menu.tsx to add settings

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,7 +11,7 @@ import {
   IonLabel,
   IonMenuToggle,
 } from '@ionic/react';
-import { home, musicalNotes, gameController } from 'ionicons/icons'; // Add home icon
+import { home, musicalNotes, gameController, settings } from 'ionicons/icons';
 
 interface MenuProps {
   contentId: string;
@@ -30,21 +30,51 @@ const Menu: React.FC<MenuProps> = ({ contentId }) => {
         <IonList>
           <IonMenuToggle autoHide={false}>
             {/* Home Link */}
-            <IonItem button routerLink="/home" routerDirection="root" detail={false}>
+            <IonItem 
+              button 
+              routerLink="/home" 
+              routerDirection="root" 
+              detail={false}
+              className="menu-item"
+            >
               <IonIcon icon={home} slot="start" color="tertiary" />
               <IonLabel>Home</IonLabel>
             </IonItem>
             
             {/* Music Player Link */}
-            <IonItem button routerLink="/music" routerDirection="root" detail={false}>
+            <IonItem 
+              button 
+              routerLink="/music" 
+              routerDirection="root" 
+              detail={false}
+              className="menu-item"
+            >
               <IonIcon icon={musicalNotes} slot="start" color="primary" />
               <IonLabel>Music Player</IonLabel>
             </IonItem>
             
             {/* Games Link */}
-            <IonItem button routerLink="/games" routerDirection="root" detail={false}>
+            <IonItem 
+              button 
+              routerLink="/games" 
+              routerDirection="root" 
+              detail={false}
+              className="menu-item"
+            >
               <IonIcon icon={gameController} slot="start" color="secondary" />
               <IonLabel>Games</IonLabel>
+            </IonItem>
+
+            {/* Settings Link */}
+            <IonItem 
+              button 
+              routerLink="/settings" 
+              routerDirection="root" 
+              detail={false}
+              className="menu-item"
+            >
+              <IonIcon icon={settings} slot="start" color="medium" />
+              <IonLabel>Settings</IonLabel>
             </IonItem>
           </IonMenuToggle>
         </IonList>
